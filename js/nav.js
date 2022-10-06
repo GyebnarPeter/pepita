691d34ffa272cd7b9382af6ce7ce4795f0310fca
GyebnarPeter <gyebnarpetter@gmail.com>
2022-10-06 17:02:43 +0200

    collections page with tiles

--- js/nav.js
+++ js/nav.js
@@ -1,13 +1,16 @@
-const navBtn = document.querySelectorAll(".nav-btn");
-navBtn.forEach((btn) => {
-    btn.onclick = () => {
+const toggleNav = () => {
+    // if you click on any navigation button, the menu floats in and shifts the content of the page
+    document.querySelector(".nav").classList.toggle("nav-open");
+    document.querySelector(".content").classList.toggle("content-offset");
 
-        // if you click on any navigation button, the menu floats in and shifts the content of the page
-        document.querySelector(".nav").classList.toggle("nav-open");
-        document.querySelector(".content").classList.toggle("content-offset");
+    // the logo disappears and a button to close navigation appears instead
+    document.querySelectorAll(".toggle-item").forEach( item => {
+        item.classList.toggle("display");
+    })
+}
 
-        // the logo disappears and a button to close navigation appears instead
-        document.querySelector(".header__logo").classList.toggle("display");
-        document.querySelector(".nav-btn--hide").classList.toggle("display");
-    };
-});
+const navButtons = document.querySelectorAll(".nav-btn");
+
+for(const btn of navButtons) {
+    btn.addEventListener("click", toggleNav)
+};
